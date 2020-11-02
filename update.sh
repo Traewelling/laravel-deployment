a16d2f0040f56c37c183ea441d6791b7cf85dbdb
Levin Herr <github@17d.me>
2020-11-03 00:00:20 +0100

    Added backup and force-overwrite to updater. Will add cli options

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 install() {
-	folder="release"
+	folder="pfad"
 	oldpath=$( basename "$( readlink -f ${folder} )" )
 	path=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 
@@ -22,19 +22,32 @@ install() {
 	else
 		npm run dev
 	fi
-
+	
+	# Bringing current instance into maintenance mode
 	cd ../${oldpath}
 	php artisan down
+	
+	# Migrating new versions
 	cd ../${path}
 	php artisan migrate --force
-
+	
+	# Copy profile pictures if any were uploaded while compiling new version
+	cp -r -n ${oldpath}/public/uploads ${path}/public/uploads
+	
+	# Update symlink, backup old version and delete old backup 
 	cd ..
-		
 	echo -e "\n\n========================================="
-	rm -rf ${folder}
-	echo -e "\e[33mCreating Symlink \e[0m${folder}\e[33m for \e[0m${path}"
-	ln -s ${path} ${folder}
-	rm -rf ${oldpath}
+	echo -e "\e[33mCreating Symlink \e[36m${folder}\e[33m for \e[36m${path}"
+	ln -fsn ${path} "${folder}"
+	
+	if [ -L "${folder}_bak" ]; then
+		bak_path=$( basename "$( readlink -f ${folder}_bak )" )
+		echo -e "\e[33mDeleting old backup \e[36m${bak_path}\e[0m"
+		rm -rf "${bak_path}"
+	fi
+	
+	echo -e "\e[33mCreating symlink for backup\e[0m"
+	ln -fsn ${oldpath} "${folder}_bak"
 	cd ${folder}
 	php artisan up
 	echo -e "\e[32mDone. :)\e[0m"
