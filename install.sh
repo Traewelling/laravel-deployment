6480f4b65cafd22e6f81adf99841811e27839b02
Levin Herr <github@17d.me>
2020-11-03 12:20:54 +0100

    added comments and confirmation to install.sh

--- install.sh
+++ install.sh
@@ -9,12 +9,12 @@ force=0
 
 print_help() {
 # Display Help
-   echo "Installer for Träwelling"
+   echo "Installer for Laravel projects"
    echo
    echo "Syntax: install.sh [-|h|d|t|f] [-b BRANCHNAME] [-r REPO_URL]"
    echo "options:"
    echo "  -d"
-   echo -e "\tInstalls with seeding in the migration"
+   echo -e "\tInstalls with seeding in the migration and npm run dev"
    echo "  -t"
    echo -e "\tChecks out the latest tag"
    echo "  -f"
@@ -31,6 +31,7 @@ print_help() {
 
 install() {
 	
+	# Print out settings	
 	echo -e "\e[33mUsing branch \e[36m${branch}\e[0m"
 	echo -e "\e[33mUsing path \e[36m${folder}\e[0m"
 	if [ ${dev} == 1 ]; then
@@ -40,6 +41,7 @@ install() {
 		echo -e "\e[33mUsing latest tag\e[0m"
 	fi
 	
+	# Ask for confirmation, if force tag is not set
 	if [ ${force} == 0 ]; then
 		read -p "Are all settings correct? (Y/n)" -n 1 -r
 		echo   
@@ -55,26 +57,36 @@ install() {
 		echo -e "${folder}\e[31m already exists. Did you mean to run the updater?"
 		exit 1
 	fi
-
+	
+	# cloning project into new path and checking out branch
 	echo -e "\e[33mCloning project\e[0m"
 	git clone ${repo} ${path}
 	cd ${path}
 	git checkout ${branch}
 	
+	# Checking out latest tag if -t is set
 	if [ ${tag} ]; then
 		latesttag=$(git describe --tags --abbrev=0)
 		echo -e "\e[33mChecking out ${latesttag}\e[0m"
 		git checkout ${latesttag} -q
 	fi
+
+	# Generating .env and waiting for user input
 	cp .env.example .env
 
 	echo -e "\n\n========================================="
 	echo -e "\e[32mPlease edit the \e[0m.env\e[32m file iside the directory\e[36m${path}\e[32m to fit your needs and then continue\e[0m"
 	echo -e "\e[5mPress any key to continue\e[0m"	
 	read -n 1 -s -r -p ""
+
+	# Installing composer and npm packages
 	composer install
 	npm install
+	
+	# Generating application key
 	php artisan key:generate
+
+	# running migration npm compilation depending on dev option
 	if [ ${dev} == 0 ]; then
 		npm run prod
 		php artisan migrate
@@ -82,9 +94,12 @@ install() {
 		npm run dev
 		php artisan migrate --seed
 	fi
+
+	# Installing laravel passport
 	php artisan passport:install
-	cd ..
 	
+	# Creating symlink
+	cd ..
 	echo -e "\n\n========================================="
 	echo -e "\e[33mCreating Symlink \e[0m${folder}\e[33m for \e[0m${path}"
 	ln -s ${path} ${folder}
