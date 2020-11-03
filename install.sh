6b01527c771c9bf63a99ff1a51bd7698866f33fe
Levin Herr <github@17d.me>
2020-11-03 12:20:37 +0100

    added flags and help to update.sh

--- install.sh
+++ install.sh
@@ -2,44 +2,67 @@
 
 folder="release"
 branch="develop"
+repo="https://github.com/Traewelling/traewelling.git"
 dev=0
+tag=0
+force=0
 
 print_help() {
 # Display Help
    echo "Installer for Träwelling"
    echo
-   echo "Syntax: install.sh [-|h|d] [-b BRANCHNAME]"
+   echo "Syntax: install.sh [-|h|d|t|f] [-b BRANCHNAME] [-r REPO_URL]"
    echo "options:"
    echo "  -d"
-   echo -e "\tInstalls with seeding in the migration and loads"
-   echo -e "\tthe most recent commit instead of the latest release"
-   echo "  -h"
-   echo -e "\tPrint this Help."
+   echo -e "\tInstalls with seeding in the migration"
+   echo "  -t"
+   echo -e "\tChecks out the latest tag"
+   echo "  -f"
+   echo -e "\tSkips confirmation of settings at begin of script"
+   echo "  -r REPO_URL"
+   echo -e "\tSets the url for the to be installed repo"
    echo "  -b BRANCHNAME"
    echo -e "\tChecks out the repo with the supplied branch"
+   echo "  -h"
+   echo -e "\tPrint this Help."
+
    exit 
 }
 
 install() {
-
+	
 	echo -e "\e[33mUsing branch \e[36m${branch}\e[0m"
 	echo -e "\e[33mUsing path \e[36m${folder}\e[0m"
 	if [ ${dev} == 1 ]; then
-		echo -e "\e[33mUsing development options"
+		echo -e "\e[33mUsing development options\e[0m"
 	fi
-
+	if [ ${tag} == 1 ]; then
+		echo -e "\e[33mUsing latest tag\e[0m"
+	fi
+	
+	if [ ${force} == 0 ]; then
+		read -p "Are all settings correct? (Y/n)" -n 1 -r
+		echo   
+		if [[ $REPLY =~ ^[Nn]$ ]]
+		then
+		    exit
+		fi
+	fi
+	
+	# Creating new pathname and checking for existing symlink
 	path=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 	if [[ -L "$folder" ]]; then
 		echo -e "${folder}\e[31m already exists. Did you mean to run the updater?"
 		exit 1
 	fi
 
-	echo -e "\e[33mCloning traewelling\e[0m"
-	git clone https://github.com/Traewelling/traewelling.git ${path}
+	echo -e "\e[33mCloning project\e[0m"
+	git clone ${repo} ${path}
 	cd ${path}
 	git checkout ${branch}
-	latesttag=$(git describe --tags --abbrev=0)
-	if [ ${dev} == 0 ]; then
+	
+	if [ ${tag} ]; then
+		latesttag=$(git describe --tags --abbrev=0)
 		echo -e "\e[33mChecking out ${latesttag}\e[0m"
 		git checkout ${latesttag} -q
 	fi
@@ -68,20 +91,21 @@ install() {
 	echo -e "\e[32mDone. :)\e[0m"
 }
 
-#	install
-
 
 # --------
 # Get options
 # --------
 
-while getopts e:b:p:dh OPT
+while getopts e:b:p:r:dhtf OPT
 do
     case "$OPT" in
         h) print_help ;;
         b) branch=$OPTARG;;
 	p) folder=$OPTARG;;
+	r) repo=$OPTARG;;
 	d) dev=1;;
+	t) tag=1;;
+	f) force=1;;
         ?) print_help ;;
     esac
 done
--- update.sh
+++ update.sh
@@ -1,23 +1,79 @@
 #!/bin/bash
 
+folder="release"
+branch="develop"
+dev=0
+tag=0
+force=0
+
+print_help() {
+# Display Help
+   echo "Updater for Laravel projects"
+   echo
+   echo "Syntax: update.sh [-|h|d|t|f] [-b BRANCHNAME]"
+   echo "options:"
+   echo "  -d"
+   echo -e "\tUpdates with npm run dev"
+   echo "  -t"
+   echo -e "\tChecks out the latest tag"
+   echo "  -f"
+   echo -e "\tSkips confirmation of settings at begin of script"
+   echo "  -b BRANCHNAME"
+   echo -e "\tChecks out the repo with the supplied branch"
+   echo "  -h"
+   echo -e "\tPrint this Help."
+   
+   exit
+}
+
+
 install() {
-	folder="pfad"
+# Print out settings    
+        echo -e "\e[33mUsing branch \e[36m${branch}\e[0m"
+        echo -e "\e[33mUsing path \e[36m${folder}\e[0m"
+        if [ ${dev} == 1 ]; then
+                echo -e "\e[33mUsing development options\e[0m"
+        fi  
+        if [ ${tag} == 1 ]; then
+                echo -e "\e[33mUsing latest tag\e[0m"
+        fi  
+    
+        # Ask for confirmation, if force tag is not set
+        if [ ${force} == 0 ]; then
+                read -p "Are all settings correct? (Y/n)" -n 1 -r
+                echo   
+                if [[ $REPLY =~ ^[Nn]$ ]]
+                then
+                    exit
+                fi  
+        fi
+	
+	# Get name for current folder and generate new name
+	# Copy current version into working directory
 	oldpath=$( basename "$( readlink -f ${folder} )" )
 	path=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 
-	echo -e "\e[33mCopying old install from \e[0m${oldpath}\e[33m to \e[0m${path}"
+	echo -e "\e[33mCopying old install from \e[36m${oldpath}\e[33m to \e[36m${path}\e[0m"
 	cp -r ${oldpath} ${path}
-	cd ${path}
-	git checkout develop
+
+	# updating branch
+	cd ${path}	
+	git checkout ${branch}
 	git pull
-	latesttag=$(git describe --tags --abbrev=0)
-	if [ ${folder} = "release" ]; then
-		echo -e "\e[33mChecking out ${latesttag}\e[0m"
+	
+	# Checking out latest tag if -t is set	
+	if [ ${tag} ]; then
+		latesttag=$(git describe --tags --abbrev=0)
+		echo -e "\e[33mChecking out \e[36m${latesttag}\e[0m"
 		git checkout ${latesttag} -q
 	fi
+	
+	# Installing new packages in composer and npm
 	composer install
 	npm install
-	if [ ${folder} == "release" ]; then
+
+	# running npm compilation depending on dev option
+	if [ ${dev} == 0 ]; then
 		npm run prod
 	else
 		npm run dev
@@ -34,23 +90,44 @@ install() {
 	# Copy profile pictures if any were uploaded while compiling new version
 	cp -r -n ${oldpath}/public/uploads ${path}/public/uploads
 	
-	# Update symlink, backup old version and delete old backup 
+	# Update symlink to newest compiled version
 	cd ..
 	echo -e "\n\n========================================="
 	echo -e "\e[33mCreating Symlink \e[36m${folder}\e[33m for \e[36m${path}"
 	ln -fsn ${path} "${folder}"
 	
+	# Delete old backup if symlink to it exists
 	if [ -L "${folder}_bak" ]; then
 		bak_path=$( basename "$( readlink -f ${folder}_bak )" )
 		echo -e "\e[33mDeleting old backup \e[36m${bak_path}\e[0m"
 		rm -rf "${bak_path}"
 	fi
 	
+	# Update backup-symlink to last version
 	echo -e "\e[33mCreating symlink for backup\e[0m"
 	ln -fsn ${oldpath} "${folder}_bak"
+
+	# removing maintenance mode (should not be necessary)
 	cd ${folder}
 	php artisan up
 	echo -e "\e[32mDone. :)\e[0m"
 }
 
+
+# --------
+# Get options
+# --------
+
+while getopts e:b:p:dhtf OPT
+do
+    case "$OPT" in
+        h) print_help ;;
+        b) branch=$OPTARG;;
+        p) folder=$OPTARG;;
+        d) dev=1;;
+        t) tag=1;;
+        f) force=1;;
+        ?) print_help ;;
+    esac
+done
 	install
